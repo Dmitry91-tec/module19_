fb86dc1f84a5c4c9481123f4abeda36a44dbc509
Dmitry91-tec <nasdi91@yandex.ru>
2025-01-05 16:44:35 +1000

    QuerySet

--- myproject/task1/models.py
+++ myproject/task1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Bayer(models.Model):
     name = models.CharField(max_length=100)
-    balance = models.DecimalField(decimal_places=3, max_digits=3)
+    balance = models.DecimalField(max_digits=10, decimal_places=5)
     age = models.PositiveIntegerField()
 
     def __str__(self):
@@ -11,8 +11,8 @@ class Bayer(models.Model):
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    cost = models.DecimalField(max_length=100, decimal_places=3, max_digits=3)
-    size = models.DecimalField(max_length=100, decimal_places=3, max_digits=3)
+    cost = models.DecimalField(max_digits=10, decimal_places=5)
+    size = models.DecimalField(max_digits=10, decimal_places=5)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Bayer, related_name='game_buyer')
